e78b3d305d7af445b612ec12e19119c8a51e2c57
Thiago_Ferfoglia <thiagoferfoglia@gmail.com>
2025-05-13 15:27:56 -0300

    Co-authored-by: brunaagomesz <bruna.gomess@uni9.edu.br>

--- slide.js
+++ slide.js
@@ -17,3 +17,10 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#cabeçalho') 
+    header.classList.toggle('rolagem',this.window.scrollY > 0)
+})
+
+/* A class rolagem sera adicionado sempre que a janela tiver uma rolagem no eixo Y e que a posição da rolagem seja maior que 1*/
+
